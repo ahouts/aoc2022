25cd52a1c5b4981fb6f427e21ffdbe13f3a7150f
Andrew Houts <ahouts4@gmail.com>
2022-12-01 11:42:01 -0600

    day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,59 @@
+use itertools::Itertools;
+use std::{io::BufRead, str::FromStr};
+
+fn part1() -> usize {
+    std::io::BufReader::new(std::fs::File::open("day1/input.txt").unwrap())
+        .lines()
+        .collect::<Result<Vec<String>, _>>()
+        .unwrap()
+        .into_iter()
+        .map(|line| -> Result<Option<usize>, <usize as FromStr>::Err> {
+            if line.is_empty() {
+                Ok(None)
+            } else {
+                Ok(Some(line.parse::<usize>()?))
+            }
+        })
+        .collect::<Result<Vec<Option<usize>>, _>>()
+        .unwrap()
+        .into_iter()
+        .group_by(Option::is_some)
+        .into_iter()
+        .filter(|(is_some, _)| *is_some)
+        .map(|(_, group)| group.map(Option::unwrap))
+        .map(|group| group.sum::<usize>())
+        .max()
+        .unwrap()
+}
+
+fn part2() -> usize {
+    std::io::BufReader::new(std::fs::File::open("day1/input.txt").unwrap())
+        .lines()
+        .collect::<Result<Vec<String>, _>>()
+        .unwrap()
+        .into_iter()
+        .map(|line| -> Result<Option<usize>, <usize as FromStr>::Err> {
+            if line.is_empty() {
+                Ok(None)
+            } else {
+                Ok(Some(line.parse::<usize>()?))
+            }
+        })
+        .collect::<Result<Vec<Option<usize>>, _>>()
+        .unwrap()
+        .into_iter()
+        .group_by(Option::is_some)
+        .into_iter()
+        .filter(|(is_some, _)| *is_some)
+        .map(|(_, group)| group.map(Option::unwrap))
+        .map(|group| group.sum::<usize>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", part1());
+    println!("{}", part2());
 }
